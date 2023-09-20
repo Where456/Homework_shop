c6a394a88694a6f1a314790e74f99db999f62e81
Where456 <daanagrigoreva1@gmail.com>
2023-09-20 15:39:39 +0300

    commit

--- src/item.py
+++ src/item.py
@@ -1,30 +1,19 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        return self.price * self.quantity
 
-        :return: Общая стоимость товара.
-        """
-        pass
 
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("Товар 1", 10.0, 5)
+    assert item.calculate_total_price() == 50.0
+
+
+def test_apply_discount():
+    item = Item("Товар 2", 20.0, 3)
+    Item.pay_rate = 0.9
+    item.apply_discount()
+    assert item.price == 18.0
